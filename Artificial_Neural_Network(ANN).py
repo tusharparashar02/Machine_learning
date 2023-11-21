04b9faab256178e5a5274e79e2f87d4c6270a8b6
Tushar Parashar <105961195+tusharparashar02@users.noreply.github.com>
2023-11-21 09:50:00 +0530

    Update Artificial_Neural_Network(ANN).py

--- Artificial_Neural_Network(ANN).py
+++ Artificial_Neural_Network(ANN).py
@@ -66,3 +66,73 @@ plt.show()
 # Classification report
 report = classification_report(true_lables, predictions_lables, target_names=classes)
 print("Classification report: \n", report)
+
+
+# multi-layer perceptron
+
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix,classification_report
+
+# Load the data
+
+(train_images, train_labels), (test_images, test_lables) = mnist.load_data()
+
+# Flatten the images into 1D array
+
+train_images = train_images.reshape((60000, 28*28)).astype('float32')/255
+
+test_images = test_images.reshape((10000, 28*28)).astype('float32')/255
+
+#  one hot encode the labels
+
+train_labels = to_categorical(train_labels)
+test_lables = to_categorical(test_lables)
+
+# Build the multi layer preceptron model
+model = models.Sequential()
+model.add(layers.Dense(128, activation='relu', input_shape=(28*28,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+
+# predicition
+
+predicition = model.predict(test_images)
+
+predicition_labels = np.argmax(predicition, axis=1)
+
+true_labels = np.argmax(test_lables, axis=1)
+
+# confusion matrix
+
+cm = confusion_matrix(true_labels, predicition_labels)
+
+# plot confusion matrix
+
+plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+plt.title('Confusion matrix')
+
+plt.colorbar()
+classes = [str(i) for i in range(10)]
+
+tick_marks = np.arange(len(classes))
+
+plt.xticks(tick_marks, classes, rotation=45)
+
+plt.yticks(tick_marks, classes)
+
+plt.xlabel('Predicted label')
+
+plt.ylabel('True label')
+
+plt.show()
+
+# classification report
+
+print(classification_report(true_labels, predicition_labels))
