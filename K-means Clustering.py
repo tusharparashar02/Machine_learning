f194f0e40bff1e8cee112d229a69c7a6b003ad71
Tushar Parashar <105961195+tusharparashar02@users.noreply.github.com>
2023-12-04 20:16:59 +0530

    11-K-means Clustering.py

--- K-means Clustering.py
+++ K-means Clustering.py
@@ -1,1 +1,90 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import datasets                                                                                                                                                                                    iris = pd.read_csv('iris.csv')                                                                                                                                                                      x = iris.iloc[:, [1,2,3,4]]
+x  = np.array(x)
 
+# Define the model
+kmeans_model = KMeans(n_clusters=3, random_state=32932)
+# Fit into our dataset fit
+kmeans_predict = kmeans_model.fit_predict(x)
+
+kmeans_model.cluster_centers_
+
+iris['Cluster'] = kmeans_predict
+
+#Visualising the clusters
+plt.scatter(x[kmeans_predict == 0, 0], x[kmeans_predict == 0, 1], s = 100, c = 'red', label = 'Setosa')
+plt.scatter(x[kmeans_predict == 1, 0], x[kmeans_predict == 1, 1], s = 100, c = 'blue', label = 'Versicolour')
+plt.scatter(x[kmeans_predict == 2, 0], x[kmeans_predict == 2, 1], s = 100, c = 'green', label = 'Virginica')
+
+#Plotting the centroids of the clusters
+plt.scatter(kmeans_model.cluster_centers_[:, 0], kmeans_model.cluster_centers_[:,1], s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
+
+distortions = []
+K = range(1,10)
+for k in K:
+    kmeanModel = KMeans(n_clusters=k)
+    kmeanModel.fit(x)
+    distortions.append(kmeanModel.inertia_)
+
+plt.figure(figsize=(16,8))
+plt.plot(K, distortions, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Distortion')
+plt.title('The Elbow Method showing the optimal clusters')
+plt.show()
+
+//Implementation of k-means on a random sample data
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
+
+# Generate a synthetic dataset with three clusters
+X, y = make_blobs(n_samples=300, centers=3, random_state=42, cluster_std=1.0)
+
+# Standardize the features
+X_std = StandardScaler().fit_transform(X)
+
+# Determine the optimal number of clusters using the Elbow method
+inertia = []
+for k in range(1, 11):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X_std)
+    inertia.append(kmeans.inertia_)
+
+# Plot the Elbow curve
+plt.figure(figsize=(8, 6))
+plt.plot(range(1, 11), inertia, marker='o')
+plt.title('Elbow Method for Optimal k')
+plt.xlabel('Number of Clusters (k)')
+plt.ylabel('Inertia')
+plt.show()
+
+# Based on the Elbow method, let's choose an optimal value for k (e.g., 3)
+optimal_k = 5
+
+# Apply k-means clustering with the optimal k
+kmeans = KMeans(n_clusters=optimal_k, random_state=42)
+labels = kmeans.fit_predict(X_std)
+
+# Evaluate the clustering using silhouette score
+silhouette_avg = silhouette_score(X_std, labels)
+print(f'Silhouette Score for {optimal_k} clusters: {silhouette_avg:.2f}')
+
+# Visualize the clusters
+plt.figure(figsize=(10, 8))
+scatter = plt.scatter(X_std[:, 0], X_std[:, 1], c=labels, cmap='viridis', edgecolors='k', s=50)
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', marker='X', s=200, label='Centroids')
+plt.title('K-Means Clustering (Synthetic Dataset)')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.legend()
+plt.show()
